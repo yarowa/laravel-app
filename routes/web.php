396b918540db4dd581b7029f0036222e080318e4
yarowa <eng.yarow@gmail.com>
2020-04-14 21:16:18 +0200

    setting uo vue router and architecture of the opplication

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
+Route::view('/{any}', 'app')->where('any', '^(?!api\/)[\/\w\.-]*');
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
