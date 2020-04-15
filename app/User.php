c83ec81fea8312a62f60568118e016ee4998bfdd
yarowa <eng.yarow@gmail.com>
2020-04-15 19:10:59 +0200

    Install laravelpassport for API Authentication

--- app/User.php
+++ app/User.php
@@ -6,10 +6,11 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Str;
+use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, hasApiTokens;
 
     /**
      * The attributes that are mass assignable.
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ Route::view('/{any}', 'app')->where('any', '^(?!api\/)[\/\w\.-]*');
 /*Route::get('/', function () {
     return view('welcome');
 });*/
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
